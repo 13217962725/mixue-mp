02e11d3602e55917c18bdd6df0ff729a65c79d97
墨殇 <1651337573@qq.com>
2022-05-31 00:34:11 +0800

    首页轮播图及轮播图跳转

--- miniprogram/pages/product/detail.js
+++ miniprogram/pages/product/detail.js
@@ -1,44 +1,20 @@
-import swiper from "../../api/swiper"
+// pages/product/detail.js
 Page({
 
 	/**
 	 * 页面的初始数据
 	 */
 	data: {
-		swiperList: [],
-		current: 0
-	},
 
-	onSwiperChange(e) {
-		const {
-			current
-		} = e.detail
-		this.setData({
-			current
-		})
 	},
 
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
-	onLoad() {
-		swiper.list().then(res => {
-			this.setData({
-				swiperList: res.data
-			})
-		})
+	onLoad(options) {
+
 	},
 
-	onSwiperTab(e) {
-		const {
-			item
-		} = e.currentTarget.dataset
-		item.type === 'url' ? wx.navigateTo({
-			url: `/pages/web-view/index?url=${item.target}`,
-		}) : wx.navigateTo({
-			url: `/pages/product/detail?id=${item.target}`,
-		})
-	},
 	/**
 	 * 生命周期函数--监听页面初次渲染完成
 	 */
--- miniprogram/pages/web-view/index.js
+++ miniprogram/pages/web-view/index.js
@@ -1,44 +1,22 @@
-import swiper from "../../api/swiper"
+// pages/web-view/index.js
 Page({
 
 	/**
 	 * 页面的初始数据
 	 */
 	data: {
-		swiperList: [],
-		current: 0
-	},
-
-	onSwiperChange(e) {
-		const {
-			current
-		} = e.detail
-		this.setData({
-			current
-		})
+		url: null
 	},
 
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
-	onLoad() {
-		swiper.list().then(res => {
-			this.setData({
-				swiperList: res.data
-			})
+	onLoad(options) {
+		this.setData({
+			url: options.url
 		})
 	},
 
-	onSwiperTab(e) {
-		const {
-			item
-		} = e.currentTarget.dataset
-		item.type === 'url' ? wx.navigateTo({
-			url: `/pages/web-view/index?url=${item.target}`,
-		}) : wx.navigateTo({
-			url: `/pages/product/detail?id=${item.target}`,
-		})
-	},
 	/**
 	 * 生命周期函数--监听页面初次渲染完成
 	 */
